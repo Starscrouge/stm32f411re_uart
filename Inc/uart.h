4a6c41b3b1a4ed545ede322dc0be97a87b1043b9
PrajwalHegde <117985355+Starscrouge@users.noreply.github.com>
2026-03-05 10:27:43 +0530

    Update uart.h

--- Inc/uart.h
+++ Inc/uart.h
@@ -9,9 +9,23 @@
 #define UART_H_
 #include<stdint.h>
 #include <stm32f4xx.h>
+#define GPIOAEN (1U<<0)
+#define USART2EN (1U<<17)
+#define SYS_CLK 16000000
+#define BAUD_RATE 115200
+#define PIN2 (1U<<2)
+#define CR1_TE (1U<<3)
+#define CR1_RE (1U<<2)
+#define CR1_UE (1U<<13)
+#define SR_TXE (1U<<7)
+#define SR_RXNE (1U<<5)
+#define CR1_TXNIE (1U<<7)
+#define SR_TXE (1U<<7)
+static uint16_t compute_uart(uint32_t periphclk,uint32_t baudrate);
+static void uart_set_baud(USART_TypeDef*USARTx,uint32_t periphclk,uint32_t baudrate);
 void uart2_tx_init(void);
 void uart2_rxtx_init(void);
 void uart2_rxtx_intr(void);
-#define SR_TXE (1U<<7)
+
 
 #endif /* UART_H_ */
